84e8026e92746b761be21c4ada3da60fb529d8fe
sergio <sergio140@gmail.com>
2022-08-06 12:42:57 +0200

    Terceri

--- src/main/java/com/mycompany/primerospasos/CalculosConMath.java
+++ src/main/java/com/mycompany/primerospasos/CalculosConMath.java
@@ -12,6 +12,7 @@ public class CalculosConMath {
         System.out.println(resultado);
         
         double base=5;
+        
         double exponente=3;
         int resultado2 = (int)Math.pow(base, exponente);
         System.out.println("El resultado de "+base+ " elevado a "+exponente+ " es: " +resultado2); //(int) <- Refundición
